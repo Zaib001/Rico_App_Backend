20d84f6381f0bb3c445cbb7568b6a1e566299860
JahanZaib Malik <116706071+Zaib001@users.noreply.github.com>
2024-11-20 04:40:47 -0800

    Update app.js

--- app.js
+++ app.js
@@ -29,10 +29,7 @@ connectDB();
 
 // Middleware setup
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
